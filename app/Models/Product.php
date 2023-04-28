114b25fcb724d91e7a658211de9dec0534ba0696
vermataruna <tarunaverma792@gmail.com>
2023-04-28 17:43:28 +0530

    add properties for product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    const AVAILABLE_PRODUCT = 'available';
+    const UNAVAILABLE_PRODUCT = 'unavailable';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id'
+    ];
+
+    public function isAvailable(): bool
+    {
+        return $this->status === Product::AVAILABLE_PRODUCT;
+    }
+
 }
