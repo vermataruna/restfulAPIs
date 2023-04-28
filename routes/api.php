13e1c890092ca0aa257e31c2c2c391b425ae2b07
vermataruna <tarunaverma792@gmail.com>
2023-04-28 17:13:57 +0530

    add routes for each module

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,27 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Transaction\TransactionController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+//Buyers
+Route::resource('buyers', BuyerController::class, ['only' => ['index', 'show']]);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Categories
+Route::resource('categories', CategoryController::class, ['except' => ['create', 'edit']]);
+
+//Products
+Route::resource('products', ProductController::class, ['only' => ['index', 'show']]);
+
+//Sellers
+Route::resource('seller', SellerController::class, ['only' => ['index', 'show']]);
+
+//Transactions
+Route::resource('transaction', TransactionController::class, ['only' => ['index', 'show']]);
+
+//Users
+Route::resource('users', UserController::class, ['except' => ['create', 'edit']]);
